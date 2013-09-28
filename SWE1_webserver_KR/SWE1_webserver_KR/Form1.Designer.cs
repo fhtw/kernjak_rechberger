367e307fffdd863bd4246a57292dfedd62e10a74
Alexander Rechberger <alexander.rechberger@technikum-wien.at>
2013-09-28 16:58:08 +0200

    socket BACKUP

--- SWE1_webserver_KR/SWE1_webserver_KR/Form1.Designer.cs
+++ SWE1_webserver_KR/SWE1_webserver_KR/Form1.Designer.cs
@@ -57,7 +57,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.ClientSize = new System.Drawing.Size(282, 397);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
--- SWE1_webserver_KR/SWE1_webserver_KR/Form1.cs
+++ SWE1_webserver_KR/SWE1_webserver_KR/Form1.cs
@@ -7,18 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Folgene Namespaces werden für Sockets benötigt
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace SWE1_webserver_KR
-{
+{ 
     public partial class Form1 : Form
     {
+    
+        private TcpListener httplistener;
+        private int port = 8080;
+
+        public HTTPServer()
+        {
+
+           try 
+	{	        
+		 httplistener=new TcpListener(port);
+            httplistener.Start();
+            label1.Text="HTTPServer läuft ...";
+            Thread th = new Thread (new ThreadStart(StartListen))
+            th.Start;
+             
+	}
+	catch (Exception ex)
+	{
+		
+		MessageBox.Show(ex.Message);
+	}
+}
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {   
+        {  
+
+
             label1.Text = "Running...";
 
         }
