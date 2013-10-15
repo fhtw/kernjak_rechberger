2d2481e6c46f6f41d5096e9b2019b6eec6dfe635
dreydn <if12b037@technikum-wien.at>
2013-10-15 13:33:33 +0200

    First Interface tests

---
+++ SW1_Plugins/Plugin_temp/Plugin_temp/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin_temp
+{
+    
+
+    public class Temp : Plugin.IPlugin
+    {
+        private string text;
+
+        bool Plugin.iPLugin.checkRequest(string request)
+        {
+           if(request = "Temp"){
+               text = request;
+               return true;
+           } else {
+               return false;
+           }
+       }
+
+        string Plugin.iPLugin.handleRequest()
+        {
+            return text += "_done";
+        }
+    }
+}
